a485f6581b8678af21d714f5fbfa91381617cdd5
msbn21 <murali.leo17@gmail.com>
2023-08-20 22:15:22 -0400

    added react and added mailchimp subscription

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,16 @@ import { defineConfig } from "astro/config";
 import { settings } from "./src/data/settings";
 import sitemap from "@astrojs/sitemap";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://msbn21.github.io",
   base: "/sample-astro-landing-page",
-  integrations: [sitemap()],
+  integrations: [sitemap(), react()],
   vite: {
     ssr: {
-      external: ["svgo"],
-    },
-  },
+      external: ["svgo"]
+    }
+  }
 });
